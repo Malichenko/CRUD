2995af17f8fb9afcd2a270ecefd8fba92c7e59c4
Алексей Маличенко <malichenko.al@gmail.com>
2020-09-20 18:37:42 +0300

    Updates

---
+++ precache-manifest.bd0c6b0c03939d9a110f9161a58b1b71.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9553c0a3085cd36eaf3ddb55127f84",
+    "url": "/CRUD/index.html"
+  },
+  {
+    "revision": "42a49ada99b817ac0305",
+    "url": "/CRUD/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "e35dc0435745e48e23a0",
+    "url": "/CRUD/static/css/main.134382d6.chunk.css"
+  },
+  {
+    "revision": "42a49ada99b817ac0305",
+    "url": "/CRUD/static/js/2.b3b44295.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/CRUD/static/js/2.b3b44295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35dc0435745e48e23a0",
+    "url": "/CRUD/static/js/main.1f5ff099.chunk.js"
+  },
+  {
+    "revision": "6ec5ca3e134d4430d89b",
+    "url": "/CRUD/static/js/runtime-main.44403f8b.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/CRUD/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/CRUD/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/CRUD/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
